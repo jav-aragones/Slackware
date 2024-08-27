cb8b6b5a4231dc28876e2dd5a9f9157cae3c0956
jav-aragones <javaragones@gmail.com>
2024-08-27 17:00:01 -0300

    Add coments  sway.sh

--- sway.sh
+++ sway.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-#
-#
+#!/bin/bash 
 #exec ck-launch-session dbus-launch --sh-syntax --exit-with-session sway
-
-
+#
+#
+# Este comando se usa para iniciar sway. Permite la gestión de pendrivers.
+#
+#
 exec dbus-launch --sh-syntax --exit-with-session sway
